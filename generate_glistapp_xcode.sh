8c1cba1346c44b7b161d2b5b8bede193acdda240
sorbatdev <44715451+sorbatdev@users.noreply.github.com>
2023-08-14 16:29:17 +0300

    Changed build directory and added install script

--- generate_glistapp_xcode.sh
+++ generate_glistapp_xcode.sh
@@ -1,7 +1,12 @@
 export CC="/opt/homebrew/opt/llvm@15/bin/clang"
-export CC="/opt/homebrew/opt/llvm@15/bin/clang++"
+export CXX="/opt/homebrew/opt/llvm@15/bin/clang++"
 
-cmake . -GXcode -B build
+BINARY_DIR="./../../zbin/glistzbin-macos/mingw64/lib"
+BUILD_TYPE="Debug"
 
-cd scripts/macos
-sh install_assets.sh
+cmake . -G Xcode -B _build
+xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmod.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmod.dylib
+xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmodL.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmodL.dylib
+
+cp -r "assets" "_build/$BUILD_TYPE"
+cp "_build/_build/$BUILD_TYPE/libGlistEngine.dylib" "_build/$BUILD_TYPE"
