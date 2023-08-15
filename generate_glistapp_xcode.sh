b5ef338b3e9032d306fcf600288bacee8fb90542
sorbatdev <44715451+sorbatdev@users.noreply.github.com>
2023-08-15 14:10:55 +0300

    Added instructions for Xcode and improved script.

--- generate_glistapp_xcode.sh
+++ generate_glistapp_xcode.sh
@@ -1,12 +1,27 @@
+#!/bin/sh
+
 export CC="/opt/homebrew/opt/llvm@15/bin/clang"
 export CXX="/opt/homebrew/opt/llvm@15/bin/clang++"
 
 BINARY_DIR="./../../zbin/glistzbin-macos/mingw64/lib"
 BUILD_TYPE="Debug"
+GLISTWD="$(pwd)"
+BUILD_DIR="$GLISTWD/_build"
 
 cmake . -G Xcode -B _build
 xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmod.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmod.dylib
 xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmodL.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmodL.dylib
 
-cp -r "assets" "_build/$BUILD_TYPE"
-cp "_build/_build/$BUILD_TYPE/libGlistEngine.dylib" "_build/$BUILD_TYPE"
+if [ ! -d "$BUILD_DIR/$BUILD_TYPE" ]
+then
+    cd $BUILD_DIR
+    mkdir $BUILD_TYPE
+fi
+cd $BUILD_DIR/$BUILD_TYPE
+mkdir assets
+
+cd $GLISTWD
+
+cp -r "assets/" "_build/$BUILD_TYPE/assets/"
+
+open _build/GlistApp.xcodeproj
