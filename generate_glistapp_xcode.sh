a1efe34201faad35786ca9e30610d1ae01d143ad
Kayra Urfalı <kayraurfali@192.168.1.56>
2023-10-18 22:27:47 +0300

    Added necessary changes to work with gipIOS plugin.

--- generate_glistapp_xcode.sh
+++ generate_glistapp_xcode.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 
+source scripts/ios-secret.sh
+
+# EXPOSED VARIABLES (CAN CHANGE)
+BUILD_TYPE="Debug"
+PLATFORM=OS64 #OS64, SIMULATOR64COMBINED
+ARCHS=arm64
+DEPLOYMENT_VERSION="13.0"
+
+# EXPOSED VARIABLES END
+
 export CC="/opt/homebrew/opt/llvm/bin/clang"
 export CXX="/opt/homebrew/opt/llvm/bin/clang++"
+export OBJC="/opt/homebrew/opt/llvm/bin/clang"
+export OBJCXX="/opt/homebrew/opt/llvm/bin/clang++"
 
 BINARY_DIR="./../../zbin/glistzbin-macos/mingw64/lib"
-BUILD_TYPE="Debug"
 GLISTWD="$(pwd)"
 BUILD_DIR="$GLISTWD/_build"
 
-cmake . -G Xcode -B _build
+cmake -S . \
+-D CMAKE_TOOLCHAIN_FILE="$GLISTWD/cmake/ios.toolchain.cmake" \
+-G Xcode \
+-B _build \
+-D PLATFORM=$PLATFORM \
+-D DEPLOYMENT_TARGET=$DEPLOYMENT_VERSION \
+-D GLIST_IOS_DEVELOPMENT_TEAM=$GLIST_IOS_DEVELOPMENT_TEAM \
+-D GLIST_IOS_PRODUCT_BUNDLE_IDENTIFIER=$GLIST_IOS_PRODUCT_BUNDLE_IDENTIFIER \
+-D GLIST_IOS_PRODUCT_NAME=$GLIST_IOS_PRODUCT_NAME
+
 xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmod.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmod.dylib
 xattr -w com.apple.quarantine "$(xattr -p com.apple.quarantine "$BINARY_DIR/libfmodL.dylib" | sed 's/^.\{4\}/00c1/')" $BINARY_DIR/libfmodL.dylib
 
